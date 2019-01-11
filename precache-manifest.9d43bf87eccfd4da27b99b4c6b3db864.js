01a0b5dcbaa58e82c7972c5bc120834b689d647c
ryuros <jieun.ryu0531@gmail.com>
2019-01-11 17:36:17 +0900

    Updates

---
+++ precache-manifest.9d43bf87eccfd4da27b99b4c6b3db864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "fbf8c4805432bd79b182",
+    "url": "/movie_app/static/js/main.fbf8c480.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "fbf8c4805432bd79b182",
+    "url": "/movie_app/static/css/main.48f67f90.chunk.css"
+  },
+  {
+    "revision": "7ec43e34459e421b44a9ea17b3b4b63e",
+    "url": "/movie_app/index.html"
+  }
+];
